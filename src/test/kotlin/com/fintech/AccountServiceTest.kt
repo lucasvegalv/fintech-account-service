579cab24bc3905bba4f7737e4f5b6c189db5ca3c
Lucas Vega <lucasvega02@gmail.com>
2025-06-23 19:41:23 -0300

    feat: crear scaffolding basico e inicial

--- src/test/kotlin/com/fintech/AccountServiceTest.kt
+++ src/test/kotlin/com/fintech/AccountServiceTest.kt
@@ -1,9 +1,9 @@
 package com.fintech
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import jakarta.inject.Inject
+import org.junit.jupiter.api.Assertions.*
 
 @MicronautTest
 class AccountServiceTest {
@@ -13,7 +13,7 @@ class AccountServiceTest {
 
     @Test
     fun testItWorks() {
-        Assertions.assertTrue(application.isRunning)
+        assertTrue(application.isRunning)
     }
 
 }
---
+++ src/test/kotlin/com/fintech/contexts/customers/domain/CustomerTest.kt
@@ -0,0 +1,23 @@
+package com.fintech.contexts.customers.domain
+
+class CustomerTest {
+
+    // TODO: Should register a new customer just if he/she enters all the required values
+
+
+    // TODO: Should reject registration if there's a user already registered with the same DNI
+
+    // TODO: Should reject registration if Name field is invalid (not empty - just chars - max 50 )
+
+    // TODO: Should reject registration if Lastname field is invalid ( not empty - just chars - max 50 )
+
+    // TODO: Should reject registration if DNI field is invalid (just numbers)
+
+
+
+
+    // TODO: Should desactivate a customer just if he/she is ACTIVE
+
+    // TODO: Should reject desactivation if the customer has active accounts
+
+}
